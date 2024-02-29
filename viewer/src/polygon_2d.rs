75e890fad3bb40eb6bfea095ee2d27e80aaa2c5b
sascha-raesch <sascha.raesch@threedy.io>
2024-02-29 23:08:42 +0100

    added: fully working polygon 2D overlap area computation

--- viewer/src/polygon_2d.rs
+++ viewer/src/polygon_2d.rs
@@ -16,87 +16,79 @@ impl<const N: usize> Polygon2D<N> {
     }
 
     /// Computes the area of the polygon.
+    #[inline]
     pub fn compute_area(&self) -> f32 {
-        // Using the Shoelace formula to compute the area of the polygon.
-        // https://en.wikipedia.org/wiki/Shoelace_formula
-        self.vertices
-            .iter()
-            .zip(self.vertices.iter().cycle().skip(1))
-            .fold(0.0, |acc, (v1, v2)| acc + v1.x * v2.y - v1.y * v2.x)
-            / 2.0
+        Self::compute_area_for_given_vertices(self.vertices.as_ref())
     }
 
-    pub fn compute_area_with_overlapping_rectangle(&self, width: f32, height: f32) -> f32 {
-        let mut area = 0f32;
-
-        // Initialize index by finding the vertex that is right from the X-axis.
-        // If there is no such vertex, the polygon is completely on the left side of the X-axis.
-        // In this case, the area is 0.
-        let start_index = match self.vertices.iter().position(|v| v.x >= 0f32) {
-            Some(index) => index,
-            None => return 0f32,
-        };
-
-        // The last vertex that is right from the X-axis
-        let mut last_vertex = self.vertices[start_index];
-        let mut cur_vertex = 1;
-        while cur_vertex < N + 1 {
-            let v2 = &self.vertices[(cur_vertex + start_index) % N];
-
-            // If the next vertex is also on the right side of the x-axis
-            if v2.x >= 0f32 {
-                area += last_vertex.x * v2.y - last_vertex.y * v2.x;
-                last_vertex = *v2;
-                cur_vertex += 1;
-                continue;
-            }
-
-            // Compute first intersection point of the line with the x-axis
-            let vi1 = Self::intersection_with_axis::<0>(&last_vertex, v2, 0f32);
-
-            // Add the contribution of the line-segment defined by last_vertex and vi1
-            area += last_vertex.x * vi1.y - last_vertex.y * vi1.x;
-
-            // Find the next vertex that is right from the x-axis.
-            // Note that there must be such a vertex as the starting vertex is on the right side
-            // of the x-axis.
-            let next_vertex = (cur_vertex + 1..N + 1)
-                .find(|i| self.vertices[(i + start_index) % N].x >= 0f32)
-                .unwrap();
-
-            let vi2 = {
-                let v1 = &self.vertices[(next_vertex + start_index - 1) % N];
-                let v2 = &self.vertices[(next_vertex + start_index) % N];
-
-                Self::intersection_with_axis::<0>(v1, v2, 0f32)
-            };
-
-            area += vi1.x * vi2.y - vi1.y * vi2.x;
-
-            // update the current vertex and the last vertex that is right from the x-axis
-            last_vertex = vi2;
-            cur_vertex = next_vertex;
-        }
-
-        area / 2f32
-    }
-
-    /// Computes the intersection with the line defined by the two given vertices and the
-    /// specified axis at the given position.
+    /// Computes the intersection of the given vertices with the given axis.
     ///
     /// # Arguments
-    /// * `v1` - The first vertex of the line.
-    /// * `v2` - The second vertex of the line.
+    /// * `width` - The width of the rectangle.
+    /// * `height` - The height of the rectangle.
+    pub fn compute_area_with_overlapping_rectangle(&self, width: f32, height: f32) -> f32 {
+        let mut vertices = Self::cut_with_axis::<0>(0f32, self.vertices.as_ref(), 1f32);
+        vertices = Self::cut_with_axis::<0>(width, vertices.as_ref(), -1f32);
+        vertices = Self::cut_with_axis::<1>(0f32, vertices.as_ref(), 1f32);
+        vertices = Self::cut_with_axis::<1>(height, vertices.as_ref(), -1f32);
+        Self::compute_area_for_given_vertices(&vertices)
+    }
+
+    /// Cuts the given vertices with the given axis and removes either the negative or positive
+    /// part, depending on the
+    ///
+    /// # Arguments
     /// * `axis_offset` - The offset of the axis along the other axis.
+    /// * `in_vertices` - The vertices of the polygon to cut.
+    /// * `factor` - The factor for the defined axis to check if the vertex is on the correct side.
     #[inline]
-    fn intersection_with_axis<const A: usize>(v1: &Vec2, v2: &Vec2, axis_offset: f32) -> Vec2 {
-        let t = (axis_offset - v2[A]) / (v1[A] - v2[A]);
-        t * v1 + (1f32 - t) * v2
+    fn cut_with_axis<const A: usize>(
+        axis_offset: f32,
+        in_vertices: &[Vec2],
+        factor: f32,
+    ) -> Vec<Vec2> {
+        let mut result = Vec::new();
+
+        for (v1, v2) in in_vertices.iter().zip(in_vertices.iter().cycle().skip(1)) {
+            let x1 = v1[A] - axis_offset;
+            let x2 = v2[A] - axis_offset;
+
+            // add the vertex if it is on the correct side of the axis
+            if x1 * factor >= 0f32 {
+                result.push(*v1);
+            }
+
+            // check if the axis is intersecting
+            if x1 * x2 < 0f32 {
+                let t = x2 / (x2 - x1);
+                let vi = t * v1 + (1f32 - t) * v2;
+
+                result.push(vi);
+            }
+        }
+
+        result
+    }
+
+    /// Computes the area of the polygon defined by the given vertices.
+    ///
+    /// # Arguments
+    /// * `vertices` - The vertices of the polygon in counterclockwise order.
+    fn compute_area_for_given_vertices(vertices: &[Vec2]) -> f32 {
+        // Using the Shoelace formula to compute the area of the polygon.
+        // https://en.wikipedia.org/wiki/Shoelace_formula
+        vertices
+            .iter()
+            .zip(vertices.iter().cycle().skip(1))
+            .fold(0.0, |acc, (v1, v2)| acc + v1.x * v2.y - v1.y * v2.x)
+            / 2.0
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::f32::consts::E;
+
     use super::*;
 
     #[test]
@@ -118,19 +110,6 @@ mod tests {
             Vec2::new(0.0, 1.0),
         ]);
         assert_eq!(polygon.compute_area(), 0.5);
-    }
-
-    #[test]
-    fn test_intersection_with_axis() {
-        let v1 = Vec2::new(0.0, 0.0);
-        let v2 = Vec2::new(1.0, 1.0);
-        let axis_offset = 0.5;
-        let intersection = Polygon2D::<2>::intersection_with_axis::<0>(&v1, &v2, axis_offset);
-        assert_eq!(intersection, Vec2::new(0.5, 0.5));
-
-        let axis_offset = 0.0;
-        let intersection = Polygon2D::<2>::intersection_with_axis::<0>(&v1, &v2, axis_offset);
-        assert_eq!(intersection, Vec2::new(0.0, 0.0));
     }
 
     #[test]
@@ -156,7 +135,7 @@ mod tests {
             Vec2::new(-2.0, 1.0),
         ]);
         assert_eq!(
-            polygon.compute_area_with_overlapping_rectangle(0.0, 1.0),
+            polygon.compute_area_with_overlapping_rectangle(1.0, 1.0),
             1f32
         );
     }
@@ -176,5 +155,78 @@ mod tests {
         let half_area = polygon.compute_area_with_overlapping_rectangle(1.0, 2.0);
 
         assert!((full_area - half_area * 2f32).abs() < 1e-6);
+    }
+
+    #[test]
+    fn test_compute_area_with_overlapping_rectangle_circle_corners() {
+        const N: usize = 4;
+
+        let width = 2f32;
+        let height = 2f32;
+
+        let corners = [
+            Vec2::new(0f32, 0f32),
+            Vec2::new(width, 0f32),
+            Vec2::new(width, height),
+            Vec2::new(0f32, height),
+        ];
+
+        for (i, corner) in corners.iter().enumerate() {
+            let mut vertices = [zero(); N];
+
+            for i in 0..N {
+                let angle = 2.0 * std::f32::consts::PI * i as f32 / N as f32;
+                vertices[i] = Vec2::new(angle.cos(), angle.sin()) + corner;
+            }
+
+            let polygon = Polygon2D::new(vertices);
+            let full_area = polygon.compute_area();
+            let quarter_area = polygon.compute_area_with_overlapping_rectangle(width, height);
+
+            assert!(
+                (full_area - quarter_area * 4f32).abs() < 1e-6,
+                "Corner({})={}: Is={}, Should={}",
+                i,
+                corner,
+                quarter_area,
+                full_area / 4f32
+            );
+        }
+    }
+
+    #[test]
+    fn test_compute_area_with_overlapping_rectangle_circle_huge() {
+        const N: usize = 32;
+        let mut vertices = [zero(); N];
+
+        for i in 0..N {
+            let angle = 2.0 * std::f32::consts::PI * i as f32 / N as f32;
+            vertices[i] = Vec2::new(angle.cos(), angle.sin()) * 10f32 + Vec2::new(0.5f32, 0.5f32);
+        }
+
+        let polygon = Polygon2D::new(vertices);
+        let area = polygon.compute_area_with_overlapping_rectangle(1.0, 1.0);
+
+        assert!((area - 1f32).abs() < 1e-6);
+    }
+
+    #[test]
+    fn test_compute_area_with_overlapping_rectangle_huge() {
+        let rectangle = [
+            Vec2::new(0.5, 2.0),
+            Vec2::new(0.5, -2.0),
+            Vec2::new(2.0, -2.0),
+            Vec2::new(2.0, 2.0),
+        ];
+
+        let polygon = Polygon2D::new(rectangle);
+        let area = polygon.compute_area_with_overlapping_rectangle(1.0, 1.0);
+
+        assert!(
+            (area - 0.5f32).abs() < 1e-6,
+            "Is={}, Should={}",
+            area,
+            0.5f32
+        );
     }
 }
