dba35eeb2baeaebd03915bc3d6e8e79e8b081c2d
sascha-raesch <sascha.raesch@threedy.io>
2024-03-10 19:30:35 +0100

    changed: analyze pixels maps to a web application

---
+++ analyze_pixel_maps/src/index.ts
@@ -0,0 +1,25 @@
+import { load_from_url } from "./pixel_contrib";
+
+/**
+ * @returns {string | null} - The pixel contribution URL from the query string, or null if it is
+ *                            not present.
+ */
+function get_pixel_contributions_url(): string | null {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('pixel_contrib');
+}
+
+async function init() {
+    const url = get_pixel_contributions_url();
+    if (url) {
+        console.log(`Loading pixel contributions from ${url}`);
+        const pixel_contrib = await load_from_url(url);
+
+        console.log(`Loaded pixel contributions for ${pixel_contrib.length} maps`);
+    } else {
+        console.error('No pixel contribution URL found in query string');
+    }
+}
+
+init();
---
+++ analyze_pixel_maps/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'our project',
+            template: 'src/index.html'
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        compress: true,
+        port: 4000,
+    },
+};
