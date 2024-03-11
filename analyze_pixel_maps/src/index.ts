bc7354c9cc1b44b09e29665ca340785e9133b0d1
sascha-raesch <sascha.raesch@threedy.io>
2024-03-11 18:48:36 +0100

    added: first parts of visualising the pixel contrib maps

--- analyze_pixel_maps/src/index.ts
+++ analyze_pixel_maps/src/index.ts
@@ -1,3 +1,4 @@
+import { analyze } from "./analyze";
 import { load_from_url } from "./pixel_contrib";
 
 /**
@@ -15,8 +16,9 @@ async function init() {
     if (url) {
         console.log(`Loading pixel contributions from ${url}`);
         const pixel_contrib = await load_from_url(url);
-
         console.log(`Loaded pixel contributions for ${pixel_contrib.length} maps`);
+
+        analyze(pixel_contrib);
     } else {
         console.error('No pixel contribution URL found in query string');
     }
