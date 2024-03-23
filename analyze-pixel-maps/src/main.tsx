ac987d3f82061ba0f7b0302c3c0434b46a9e57e0
sascha-raesch <sascha.raesch@threedy.io>
2024-03-23 11:44:41 +0100

    added: WASM, build works, but dev got issues with delivering WASM content

--- analyze-pixel-maps/src/main.tsx
+++ analyze-pixel-maps/src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 import './index.css'
+import init, { greet } from 'rs-pixel-maps';
+
+init().then(() => {
+  console.log('init wasm-pack');
+  greet('from vite!');
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- analyze-pixel-maps/vite.config.ts
+++ analyze-pixel-maps/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import wasmPack from 'vite-plugin-wasm-pack';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '',
+  plugins: [react(), wasmPack('./rs-pixel-maps')],
 })
