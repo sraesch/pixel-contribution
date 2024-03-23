b136be66ac90fd3fa6d92cd8de82f0028e58cf27
sascha-raesch <sascha.raesch@threedy.io>
2024-03-23 17:05:31 +0100

    changed: moved rust library to top root level

--- analyze-pixel-maps/src/main.tsx
+++ analyze-pixel-maps/src/main.tsx
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 import './index.css'
-import * as wasm from 'rs-pixel-maps';
+import * as wasm from 'rs-analyze-pixel-maps';
 
 wasm.greet('from vite!');
-
-// init().then(() => {
-//   console.log('init wasm-pack');
-//   greet('from vite!');
-// });
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
