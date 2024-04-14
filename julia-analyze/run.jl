176262cc258d5ffd1f6807de9b90fb8f9d2b16c9
sascha-raesch <sascha.raesch@threedy.io>
2024-04-14 10:32:46 +0200

    fixed: issues in the plotting functions

--- julia-analyze/run.jl
+++ julia-analyze/run.jl
@@ -15,13 +15,13 @@ using AnalyzePixelContrib
 """
 Visualizes the pixel contribution map as a heatmap.
 """
-function visualize_heatmap(map::Array{Float32,2})
-    heatmap(map, aspect_ratio=1, c=:viridis)
+function visualize_heatmap(contrib_map::ContribMap)
+    heatmap(contrib_map.values, aspect_ratio=1, c=:viridis)
 end
 
 """
 Visualizes as heightmap using the surface function.
 """
-function visualize_3d(map::Array{Float32,2})
-    surface(map, c=:viridis)
+function visualize_3d(contrib_map::ContribMap)
+    surface(contrib_map.values, c=:viridis)
 end
